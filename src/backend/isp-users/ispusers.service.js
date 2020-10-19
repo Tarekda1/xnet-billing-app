b57fb86ef4c61776743e50fc2a2bed758a3d2e65
unknown <tare2.da@gmail.com>
2020-10-20 01:55:55 +0300

    binary tree

--- src/backend/isp-users/ispusers.service.js
+++ src/backend/isp-users/ispusers.service.js
@@ -1,307 +1,311 @@
-const config = require("config.json");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const crypto = require("crypto");
-const sendEmail = require("_helpers/send-email");
-const db = require("_helpers/db");
-const Role = require("_helpers/role");
-const moment = require("moment");
-const { async } = require("rxjs");
+const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const sendEmail = require('_helpers/send-email');
+const db = require('_helpers/db');
+const Role = require('_helpers/role');
+const moment = require('moment');
+const mongoose = require('mongoose');
+const { async } = require('rxjs');
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  update,
-  createBatchUsers,
-  delete: _delete,
-  createPackage,
-  getAllPackages,
-  updatePackage,
-  deletePackage,
-  getAllUserAccounts,
-  createUserAccount,
-  updateUserAccount,
-  getUserAccById,
-  search,
+	getAll,
+	getById,
+	create,
+	update,
+	createBatchUsers,
+	delete: _delete,
+	createPackage,
+	getAllPackages,
+	updatePackage,
+	deletePackage,
+	getAllUserAccounts,
+	createUserAccount,
+	updateUserAccount,
+	getUserAccById,
+	search
 };
 
 async function create(params) {
-  // validate
-  if (await db.User.findOne({ userName: params.userName })) {
-    throw 'Username "' + params.username + '" is already added';
-  }
-  //console.log(params);
-  const user = new db.User(params);
+	// validate
+	if (await db.User.findOne({ userName: params.userName })) {
+		throw 'Username "' + params.username + '" is already added';
+	}
+	//console.log(params);
+	const user = new db.User(params);
 
-  // save account
-  await user.save();
+	// save account
+	await user.save();
 
-  return basicDetails(user);
+	return basicDetails(user);
 }
 
 async function createBatchUsers(users) {
-  // users.forEach((user) => {
-  //   console.log(user.firstName);
-  // });
-  if (users.length === 0) {
-    return { message: "empty list" };
-  }
+	// users.forEach((user) => {
+	//   console.log(user.firstName);
+	// });
+	if (users.length === 0) {
+		return { message: 'empty list' };
+	}
 
-  try {
-    const resp = await db.User.insertMany(users);
-  } catch (err) {
-    return { message: err };
-  }
+	try {
+		const resp = await db.User.insertMany(users);
+	} catch (err) {
+		return { message: err };
+	}
 
-  return { message: "Success", status: "200" };
+	return { message: 'Success', status: '200' };
 }
 
 async function createPackage(params) {
-  // validate
-  if (await db.Package.findOne({ displayName: params.displayName })) {
-    throw 'Package "' + params.displayName + '" is already added';
-  }
+	// validate
+	if (await db.Package.findOne({ displayName: params.displayName })) {
+		throw 'Package "' + params.displayName + '" is already added';
+	}
 
-  const package = new db.Package(params);
+	const package = new db.Package(params);
 
-  // save account
-  await package.save();
+	// save account
+	await package.save();
 
-  return basicPackageDetails(package);
+	return basicPackageDetails(package);
 }
 
 async function createUserAccount(params) {
-  const billDate = params.billDate;
-  console.log(
-    `today: ${new Date(billDate).getFullYear()} Month: ${new Date(
-      billDate
-    ).getMonth()}`
-  );
-  const year = new Date(billDate).getFullYear();
-  const month = new Date(billDate).getMonth();
-  //const day = new Date(billDate).getDay();
-  // validate
-  const userAcc = await db.UserAccount.findOne({
-    user: params.user,
-    billDate: params.billDate,
-  });
-  if (userAcc) {
-    console.log(
-      `userAcc: ${JSON.stringify(userAcc)} ${new Date(
-        userAcc.billDate
-      ).getMonth()} ${new Date(userAcc.billDate).getFullYear()}`
-    );
-  }
+	const billDate = new Date(params.billDate);
+	console.log(params.user);
+	console.log(`today now: ${new Date().getFullYear()} Month: ${new Date().getUTCMonth()}`);
+	const year = new Date().getFullYear();
+	const month = new Date().getMonth();
+	//const day = new Date(billDate).getDay();
+	// validate
+	// const userAcc = await db.UserAccount.findOne({
+	//   user: params.user,
+	//   billDate: params.billDate,
+	// });
+	// const userAcc = await db.UserAccount.aggregate([
+	// 	{ $match: { billDate: { $exists: true } } },
+	// 	// { $match: { user: { $exists: true } } },
+	// 	//{ $unwind: '$userAcc' },
+	// 	// { $match: { userName: 'xxxx' } },
+	// 	{ $match: { month: new Date().getUTCMonth() + 2 } }
+	// ]);
+	// const userAcc = await db.UserAccount.aggregate([
+	// 	{ $match: { billDate: { $exists: true } } },
+	// 	{ $unwind: '$user' },
+	// 	{ $project: { firstName: 1, userName: 1 } },
+	// 	{ $match: { month: new Date().getUTCMonth() + 1 } }
+	// ]);
 
-  if (
-    userAcc &&
-    userAcc.billDate &&
-    new Date(userAcc.billDate).getMonth() == month &&
-    new Date(userAcc.billDate).getFullYear() == year
-  ) {
-    throw `User bill for this date:"${moment.utc(
-      billDate,
-      "DD-MM-YYYY",
-      true
-    )}" is already added.`;
-  }
-  const user = new db.UserAccount(params);
+	// const userAcc = await db.UserAccount.aggregate([
+	// 	{ $project: { user: 1, paid: 2, comment: 3, amount: 4, month: { $month: '$billDate' } } },
+	// 	{ $match: { user: params.user, month: billDate.getMonth() + 1 } }
+	// ]);
 
-  // save account
-  await user.save();
+	const userAcc = await db.UserAccount.aggregate([
+		{ $project: { user: 1, paid: 2, comment: 3, amount: 4, month: { $month: '$billDate' } } },
+		{
+			$match: {
+				user: mongoose.mongo.ObjectId(params.user),
+				month: 10
+			}
+		}
+	]);
 
-  return basicAccountDetails(user);
+	if (userAcc) {
+		console.log(`userAcc now: ${JSON.stringify(userAcc)}`);
+	}
+
+	if (userAcc.length > 0) {
+		throw `User bill for this date:"${moment.utc(billDate, 'DD-MM-YYYY', true)}" is already added.`;
+	}
+	const user = new db.UserAccount(params);
+
+	// save account
+	await user.save();
+
+	return basicAccountDetails(user);
 }
 
 function basicPackageDetails(package) {
-  const { id, displayName, created, updated } = package;
-  return { id, displayName, created, updated };
+	const { id, displayName, created, updated } = package;
+	return { id, displayName, created, updated };
 }
 
 function basicAccountDetails(userAccount) {
-  console.log(userAccount);
-  const { user, amount, comment, paid, billDate, id } = userAccount;
-  return { user, amount, comment, paid, billDate, id };
+	console.log(userAccount);
+	const { user, amount, comment, paid, billDate, id } = userAccount;
+	return { user, amount, comment, paid, billDate, id };
 }
 
 function basicDetails(user) {
-  console.log(JSON.stringify(user));
-  const {
-    id,
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    address,
-    isUserActive,
-    created,
-    updated,
-    package,
-    userName,
-    password,
-  } = user;
-  return {
-    id,
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    address,
-    isUserActive,
-    created,
-    updated,
-    package,
-    userName,
-    password,
-  };
+	console.log(JSON.stringify(user));
+	const {
+		id,
+		firstName,
+		lastName,
+		email,
+		phoneNumber,
+		address,
+		isUserActive,
+		created,
+		updated,
+		package,
+		userName,
+		password
+	} = user;
+	return {
+		id,
+		firstName,
+		lastName,
+		email,
+		phoneNumber,
+		address,
+		isUserActive,
+		created,
+		updated,
+		package,
+		userName,
+		password
+	};
 }
 
 async function getAll() {
-  const users = await db.User.find({ isDeleted: false }).populate("package");
-  return users.map((x) => basicDetails(x));
+	const users = await db.User.find({ isDeleted: false }).populate('package');
+	return users.map((x) => basicDetails(x));
 }
 
 async function getAllUserAccounts() {
-  const users = await db.UserAccount.find()
-    .populate("user")
-    .populate("package");
-  return users.map((x) => {
-    return basicAccountDetails(x);
-  });
+	const users = await db.UserAccount.find().populate('user').populate('package');
+	return users.map((x) => {
+		return basicAccountDetails(x);
+	});
 }
 
 async function getUserAccById(id) {
-  if (!db.isValidId(id)) throw "UserAcc not found";
-  const userAcc = await db.UserAccount.findById(id)
-    .populate("user")
-    .populate("package");
-  if (!userAcc) throw "User account not found";
-  return userAcc;
+	if (!db.isValidId(id)) throw 'UserAcc not found';
+	const userAcc = await db.UserAccount.findById(id).populate('user').populate('package');
+	if (!userAcc) throw 'User account not found';
+	return userAcc;
 }
 
 async function search(searchterm) {
-  if (!searchterm) {
-    throw "Invalid search term";
-  }
-  const userAccs = db.UserAccount.aggregate([
-    { $unwind: "$user" },
-    {
-      $lookup: {
-        from: "user",
-        localField: "user",
-        foreignField: "_id",
-        as: "user",
-      },
-    },
-    {
-      $match: {
-        "user.firstName": { $regex: /searchterm/ },
-        "user.lastName": { $regex: /searchterm/ },
-      },
-    },
-  ]);
+	if (!searchterm) {
+		throw 'Invalid search term';
+	}
+	const userAccs = db.UserAccount.aggregate([
+		{ $unwind: '$user' },
+		{
+			$lookup: {
+				from: 'user',
+				localField: 'user',
+				foreignField: '_id',
+				as: 'user'
+			}
+		},
+		{
+			$match: {
+				'user.firstName': { $regex: /searchterm/ },
+				'user.lastName': { $regex: /searchterm/ }
+			}
+		}
+	]);
 
-  console.log(userAccs);
-  if (!userAccs) throw "User account not found";
-  return userAccs.map((x) => {
-    return basicAccountDetails(x);
-  });
+	console.log(userAccs);
+	if (!userAccs) throw 'User account not found';
+	return userAccs.map((x) => {
+		return basicAccountDetails(x);
+	});
 }
 
 async function getAllPackages() {
-  const packages = await db.Package.find();
-  return packages.map((x) => basicPackageDetails(x));
+	const packages = await db.Package.find();
+	return packages.map((x) => basicPackageDetails(x));
 }
 
 async function getById(id) {
-  const user = await getUser(id);
-  return basicDetails(user);
+	const user = await getUser(id);
+	return basicDetails(user);
 }
 
 async function getUser(id) {
-  if (!db.isValidId(id)) throw "User not found";
-  const user = await db.User.findById(id).populate("package");
-  if (!user) throw "User not found";
-  return user;
+	if (!db.isValidId(id)) throw 'User not found';
+	const user = await db.User.findById(id).populate('package');
+	if (!user) throw 'User not found';
+	return user;
 }
 
 async function getPackage(id) {
-  if (!db.isValidId(id)) throw "Package not found";
-  const package = await db.Package.findById(id);
-  if (!package) throw "Package not found";
-  return package;
+	if (!db.isValidId(id)) throw 'Package not found';
+	const package = await db.Package.findById(id);
+	if (!package) throw 'Package not found';
+	return package;
 }
 
 async function update(id, params) {
-  const user = await getUser(id);
+	const user = await getUser(id);
 
-  // validate
-  if (
-    user.username !== params.username &&
-    (await db.User.findOne({ username: params.username }))
-  ) {
-    throw 'Username "' + params.username + '" is already taken';
-  }
+	// validate
+	if (user.username !== params.username && (await db.User.findOne({ username: params.username }))) {
+		throw 'Username "' + params.username + '" is already taken';
+	}
 
-  // hash password if it was entered
-  if (params.password) {
-    params.passwordHash = hash(params.password);
-  }
+	// hash password if it was entered
+	if (params.password) {
+		params.passwordHash = hash(params.password);
+	}
 
-  // copy params to account and save
-  Object.assign(user, params);
-  user.updated = Date.now();
-  await user.save();
+	// copy params to account and save
+	Object.assign(user, params);
+	user.updated = Date.now();
+	await user.save();
 
-  return basicDetails(user);
+	return basicDetails(user);
 }
 
 async function updateUserAccount(id, params) {
-  const userAcc = await getUserAccById(id);
-  console.log(userAcc);
-  console.log(`params: ${params.paid}`);
-  console.log(`params: ${params.amount}`);
-  // copy params to account and save
-  Object.assign(userAcc, params);
-  userAcc.paid = params.paid;
-  userAcc.amount = Number(params.amount);
-  userAcc.updated = Date.now();
-  await userAcc.save();
+	const userAcc = await getUserAccById(id);
+	console.log(userAcc);
+	console.log(`params: ${params.paid}`);
+	console.log(`params: ${params.amount}`);
+	// copy params to account and save
+	Object.assign(userAcc, params);
+	userAcc.paid = params.paid;
+	userAcc.amount = Number(params.amount);
+	userAcc.updated = Date.now();
+	await userAcc.save();
 
-  return basicAccountDetails(userAcc);
+	return basicAccountDetails(userAcc);
 }
 
 async function updatePackage(id, params) {
-  const package = await getPackage(id);
+	const package = await getPackage(id);
 
-  // validate
-  if (
-    package.displayName !== params.displayName &&
-    (await db.Package.findOne({ displayName: params.displayName }))
-  ) {
-    throw 'Display name "' + params.displayName + '" is already taken';
-  }
+	// validate
+	if (package.displayName !== params.displayName && (await db.Package.findOne({ displayName: params.displayName }))) {
+		throw 'Display name "' + params.displayName + '" is already taken';
+	}
 
-  // copy params to account and save
-  Object.assign(package, params);
-  package.updated = Date.now();
-  await package.save();
+	// copy params to account and save
+	Object.assign(package, params);
+	package.updated = Date.now();
+	await package.save();
 
-  return basicPackageDetails(user);
+	return basicPackageDetails(user);
 }
 
 async function _delete(id) {
-  const user = await getUser(id);
-  user.isDeleted = true;
-  await user.save();
+	const user = await getUser(id);
+	user.isDeleted = true;
+	await user.save();
 }
 
 async function deletePackage(id) {
-  const pkg = await getPackage(id);
-  await pkg.remove();
+	const pkg = await getPackage(id);
+	await pkg.remove();
 }
 
 function hash(password) {
-  return bcrypt.hashSync(password, 10);
+	return bcrypt.hashSync(password, 10);
 }
